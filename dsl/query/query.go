0f3d73c5b6b11893af619aea23eb8c50f99c2885
gernest <geofreyernest@live.com>
2024-07-06 01:02:03 +0300

    add And and Or query

--- dsl/query/query.go
+++ dsl/query/query.go
@@ -9,3 +9,61 @@ import (
 type Filter interface {
 	Apply(tx *tx.Tx, columns *rows.Row) (*rows.Row, error)
 }
+
+type Noop struct{}
+
+func (Noop) Apply(tx *tx.Tx, columns *rows.Row) (*rows.Row, error) {
+	return rows.NewRow(), nil
+}
+
+type And []Filter
+
+func (a And) Apply(tx *tx.Tx, columns *rows.Row) (*rows.Row, error) {
+	switch len(a) {
+	case 0:
+		return rows.NewRow(), nil
+	case 1:
+		return a[0].Apply(tx, columns)
+	default:
+		r, err := a[0].Apply(tx, columns)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, x := range a[1:] {
+			if r.IsEmpty() {
+				return r, nil
+			}
+			n, err := x.Apply(tx, columns)
+			if err != nil {
+				return nil, err
+			}
+			r = r.Intersect(n)
+		}
+		return r, nil
+	}
+}
+
+type Or []Filter
+
+func (a Or) Apply(tx *tx.Tx, columns *rows.Row) (*rows.Row, error) {
+	switch len(a) {
+	case 0:
+		return rows.NewRow(), nil
+	case 1:
+		return a[0].Apply(tx, columns)
+	default:
+		r, err := a[0].Apply(tx, columns)
+		if err != nil {
+			return nil, err
+		}
+		for _, x := range a[1:] {
+			n, err := x.Apply(tx, columns)
+			if err != nil {
+				return nil, err
+			}
+			r = r.Union(n)
+		}
+		return r, nil
+	}
+}
