87861e78b503d4e32dfe0f47d5d5f249f1502472
gernest <geofreyernest@live.com>
2024-07-06 00:08:30 +0300

    add distinct

--- dsl/tx/tx.go
+++ dsl/tx/tx.go
@@ -1,6 +1,8 @@
 package tx
 
 import (
+	"fmt"
+
 	"github.com/gernest/rbf"
 	"github.com/gernest/rbf/dsl/tr"
 )
@@ -10,4 +12,17 @@ type Tx struct {
 	Shard uint64
 	View  string
 	Tr    *tr.Read
+}
+
+func (tx *Tx) Key(field string) string {
+	return fmt.Sprintf("~%s;%s<", field, tx.View)
+}
+
+func (tx *Tx) Cursor(field string, f func(c *rbf.Cursor, tx *Tx) error) error {
+	c, err := tx.Tx.Cursor(tx.Key(field))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	return f(c, tx)
 }
