11bc783cd9bfed37af6244f33f0a8f1bf9dee086
gernest <geofreyernest@live.com>
2024-07-06 02:21:14 +0300

    count bool columns

--- dsl/boolean/bool_filter.go
+++ dsl/boolean/bool_filter.go
@@ -1,6 +1,7 @@
 package boolean
 
 import (
+	"github.com/gernest/rbf"
 	"github.com/gernest/rbf/dsl/cursor"
 	"github.com/gernest/rbf/dsl/query"
 	"github.com/gernest/rbf/dsl/tx"
@@ -37,4 +38,26 @@ func (m *Match) Apply(tx *tx.Tx, columns *rows.Row) (*rows.Row, error) {
 		r = r.Intersect(columns)
 	}
 	return r, nil
+}
+
+func Count(txn *tx.Tx, field string, isTrue bool, columns *rows.Row) (count int64, err error) {
+	err = txn.Cursor(field, func(c *rbf.Cursor, tx *tx.Tx) error {
+		var r *rows.Row
+		var err error
+		if isTrue {
+			r, err = cursor.Row(c, tx.Shard, trueRowID)
+
+		} else {
+			r, err = cursor.Row(c, tx.Shard, falseRowID)
+		}
+		if err != nil {
+			return err
+		}
+		if columns != nil {
+			r = r.Intersect(columns)
+		}
+		count = int64(r.Count())
+		return nil
+	})
+	return
 }
