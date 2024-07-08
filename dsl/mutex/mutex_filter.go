70bfdb8e4e24927c4a010617023efbd4f301787e
gernest <geofreyernest@live.com>
2024-07-08 03:18:18 +0300

    match blobs

--- dsl/mutex/mutex_filter.go
+++ dsl/mutex/mutex_filter.go
@@ -3,7 +3,6 @@ package mutex
 import (
 	"errors"
 	"io"
-	"strings"
 
 	"github.com/gernest/rbf"
 	"github.com/gernest/rbf/dsl/cursor"
@@ -61,9 +60,9 @@ func (m *MatchString) Apply(txn *tx.Tx, columns *rows.Row) (r *rows.Row, err err
 	err = txn.Cursor(m.Field, func(c *rbf.Cursor, tx *tx.Tx) error {
 		switch m.Op {
 		case EQ:
-			r, err = m.eq(c, tx, columns)
+			r, err = eq(c, m.Field, []byte(m.Value), tx, columns)
 		case NEQ:
-			r, err = m.neq(c, tx, columns)
+			r, err = neq(c, m.Field, []byte(m.Value), tx, columns)
 		case RE:
 			r, err = m.re(c, tx, columns)
 		case NRE:
@@ -76,19 +75,19 @@ func (m *MatchString) Apply(txn *tx.Tx, columns *rows.Row) (r *rows.Row, err err
 	return
 }
 
-func (m *MatchString) neq(c *rbf.Cursor, tx *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
-	exists, err := m.existence(tx)
+func neq(c *rbf.Cursor, field string, key []byte, tx *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
+	exists, err := existence(tx)
 	if err != nil {
 		return nil, err
 	}
-	r, err = m.eq(c, tx, columns)
+	r, err = eq(c, field, key, tx, columns)
 	if err != nil {
 		return nil, err
 	}
 	return exists.Difference(r), nil
 }
 
-func (m *MatchString) existence(txn *tx.Tx) (r *rows.Row, err error) {
+func existence(txn *tx.Tx) (r *rows.Row, err error) {
 	err = txn.Cursor("_id", func(c *rbf.Cursor, tx *tx.Tx) error {
 		r, err = cursor.Row(c, tx.Shard, 0)
 		return err
@@ -96,15 +95,15 @@ func (m *MatchString) existence(txn *tx.Tx) (r *rows.Row, err error) {
 	return
 }
 
-func (m *MatchString) eq(c *rbf.Cursor, tx *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
-	id, ok := tx.Tr.Find(m.Field, []byte(m.Value))
+func eq(c *rbf.Cursor, field string, key []byte, tx *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
+	id, ok := tx.Tr.Find(field, key)
 	if !ok {
 		return rows.NewRow(), nil
 	}
-	return m.eqID(c, tx, id, columns)
+	return eqID(c, tx, id, columns)
 }
 
-func (m *MatchString) eqID(c *rbf.Cursor, tx *tx.Tx, id uint64, columns *rows.Row) (r *rows.Row, err error) {
+func eqID(c *rbf.Cursor, tx *tx.Tx, id uint64, columns *rows.Row) (r *rows.Row, err error) {
 	r, err = cursor.Row(c, tx.Shard, id)
 	if err != nil {
 		return
@@ -116,7 +115,7 @@ func (m *MatchString) eqID(c *rbf.Cursor, tx *tx.Tx, id uint64, columns *rows.Ro
 }
 
 func (m *MatchString) nre(c *rbf.Cursor, tx *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
-	exists, err := m.existence(tx)
+	exists, err := existence(tx)
 	if err != nil {
 		return nil, err
 	}
@@ -128,8 +127,8 @@ func (m *MatchString) nre(c *rbf.Cursor, tx *tx.Tx, columns *rows.Row) (r *rows.
 }
 
 func (m *MatchString) re(c *rbf.Cursor, tx *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
-	err = tx.Tr.SearchRe(m.Field, clean(m.Value), nil, nil, func(key []byte, value uint64) error {
-		n, err := m.eqID(c, tx, value, columns)
+	err = tx.Tr.SearchRe(m.Field, m.Value, nil, nil, func(key []byte, value uint64) error {
+		n, err := eqID(c, tx, value, columns)
 		if err != nil {
 			return err
 		}
@@ -149,8 +148,25 @@ func (m *MatchString) re(c *rbf.Cursor, tx *tx.Tx, columns *rows.Row) (r *rows.R
 	return
 }
 
-func clean(r string) string {
-	r = strings.TrimPrefix(r, "$")
-	r = strings.TrimSuffix(r, "^")
-	return r
+type Blob struct {
+	Field string
+	Op    OP
+	Value []byte
+}
+
+var _ query.Filter = (*Blob)(nil)
+
+func (b *Blob) Apply(txn *tx.Tx, columns *rows.Row) (r *rows.Row, err error) {
+	err = txn.Cursor(b.Field, func(c *rbf.Cursor, tx *tx.Tx) error {
+		switch b.Op {
+		case EQ:
+			r, err = eq(c, b.Field, b.Value, txn, columns)
+		case NEQ:
+			r, err = neq(c, b.Field, b.Value, txn, columns)
+		default:
+			r = rows.NewRow()
+		}
+		return err
+	})
+	return
 }
